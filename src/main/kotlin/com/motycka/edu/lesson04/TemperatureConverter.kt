19fcca408484f78945bd5e5d405a43becf34a78b
1453nicat <nicat.akhmedoov@gmail.com>
2025-06-26 20:19:28 +0400

    lesson4 solution

--- src/main/kotlin/com/motycka/edu/lesson04/TemperatureConverter.kt
+++ src/main/kotlin/com/motycka/edu/lesson04/TemperatureConverter.kt
@@ -1,33 +1,11 @@
 package com.motycka.edu.lesson04
 
-import io.github.oshai.kotlinlogging.KotlinLogging
-
-private val logger = KotlinLogging.logger { }
-
-/**
- * Class for converting temperature values between Fahrenheit and Celsius
- */
 class TemperatureConverter {
-
-    /**
-     * Converts temperature value given in Fahrenheit to Celsius
-     *
-     * @param fahrenheit temperature value in Fahrenheit
-     * @return temperature value in Celsius
-     */
     fun toCelsius(fahrenheit: Double): Double {
-        logger.info { "Converting $fahrenheit°F to Celsius" }
         return (fahrenheit - 32) * 5.0 / 9.0
     }
 
-    /**
-     * Converts temperature value given in Celsius to Fahrenheit
-     *
-     * @param celsius temperature value in Celsius
-     * @return temperature value in Fahrenheit
-     */
     fun toFahrenheit(celsius: Double): Double {
-        logger.info { "Converting $celsius°C to Fahrenheit" }
         return celsius * 9.0 / 5.0 + 32
     }
 }
